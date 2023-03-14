0c1c09cfd02afbb6533ba7d6725c9d00f662d6e3
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-14 13:51:39 +0300

    Add project files.

---
+++ MVCCodeFirst_1/Models/ContextClasses/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using MVCCodeFirst_1.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCCodeFirst_1.Models.ContextClasses
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Category>  Categories { get; set; }
+    }
+}
---
+++ MVCCodeFirst_1/Models/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCodeFirst_1.Models.Entities
+{
+    public class Category:BaseEntity
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
